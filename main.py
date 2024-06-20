1c57eaa52b7ee324cf47494bb0a4f58847eac089
d3vmeh <devxmehra@gmail.com>
2024-06-19 23:39:25 -0700

    Splitting chunks

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
 
 def load_doc(path):
     doc_loader = PyPDFDirectoryLoader(path)
     return doc_loader.load()
 
+
+def split_docs(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 600, chunk_overlap = 60, length_function = len, is_separator_regex  = False)
+    return text_splitter.split_documents(documents)
+
+
 document = load_doc("")
-print(document[0])
+chunks = split_docs(document)
