765cf4d5203b25312a7a17e96c3bd84fb747bcc8
d3vmeh <devxmehra@gmail.com>
2024-06-20 01:50:54 -0700

    Learning based on conversation and essay PDF provided and embedded using OpenAI Embeddings

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema.document import Document
+from database import *
 
-def load_doc(path):
-    doc_loader = PyPDFDirectoryLoader(path)
-    return doc_loader.load()
-
-
-def split_docs(documents: list[Document]):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 600, chunk_overlap = 60, length_function = len, is_separator_regex  = False)
-    return text_splitter.split_documents(documents)
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.embeddings.gpt4all import GPT4AllEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 
-document = load_doc("")
-chunks = split_docs(document)
+from langchain_community.llms import Ollama
+
+
+chunks = create_chunks("",replace_newlines=True)
+
+
+#embeddings = OllamaEmbeddings(model="llama3")
+#embeddings = BedrockEmbeddings(credentials_profile_name="default",region_name="us-east-1")
+#embeddings = GPT4AllEmbeddings()
+embeddings = OpenAIEmbeddings()
+
+#save_database(embeddings,chunks)
+
+db = load_database(embeddings)
+
+model = Ollama(
+    model = "llama3"
+)
+
+
+
+count = 1
+while True:
+    query = input("Enter a query: ")
+    if query.lower() == "exit":
+         break
+    
+
+    results = query_database(query, db, num_responses = 5)
+
+    f = open("conversations.txt",'r')
+    conversations = f.read()
+    f.close()
+
+    prompt = """
+    Answer the question only based on previous conversations with the user, where greater context weight means greater relevance to the conversation, and the following context:
+
+
+    Here are the previous conversations with the user:
+    {conversations}
+
+
+    Here is the context you can use:
+    {context}
+
+    ------------
+
+
+    Answer the question based on previous conversations with the user, where greater context weight means greater relevance to the conversation, and the above context: {question}"""
+
+    response = get_response(query, results, prompt, model,conversations=conversations)
+
+    print(f"\n{response}")
+
+    file = open("conversations.txt",'w')
+    file.write(f"\nContext weight: {count}\nUser question: {query}\nYour response:{response}\n")
+    file.close()
+
+    count += 1
